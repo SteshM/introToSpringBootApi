52b9af5d27475ae50fd62d26737615afd625854f
SteshM <mbithestella0@gmail.com>
2023-06-07 12:13:58 +0300

    logged my application

--- src/main/java/com/example/introToSpringBBootApi/Controller/EmployeeController.java
+++ src/main/java/com/example/introToSpringBBootApi/Controller/EmployeeController.java
@@ -4,6 +4,8 @@ import com.example.introToSpringBBootApi.DTO.Employee;
 import com.example.introToSpringBBootApi.Entities.EmployeeEntity;
 import com.example.introToSpringBBootApi.Repository.EmployeeRepository;
 import com.example.introToSpringBBootApi.Service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import java.util.Map;
 @RequestMapping(path = "api/v1")
 
 public class EmployeeController {
+    Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 
     @Autowired
     public EmployeeService employeeService;
@@ -25,8 +28,9 @@ public class EmployeeController {
 
     @PostMapping("/employee")
     public ResponseEntity<?> createEmployee(@RequestBody Employee employee){
-
+         logger.info("about creating employee list");
        EmployeeEntity record =  employeeService.createEmployee(employee);
+       logger.error("returning null in some fields");
 
        return new ResponseEntity<>(record, HttpStatus.CREATED);
     }
